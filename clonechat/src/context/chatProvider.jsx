228aaae6b47b92f7571c1867149d4597f9cf054d
Said Meza <176p0393@itspozarica.edu.mx>
2026-07-22 00:28:50 -0600

    create chatprovider

--- clonechat/src/context/chatProvider.jsx
+++ clonechat/src/context/chatProvider.jsx
@@ -1,1 +1,16 @@
+import { useReducer } from "react"
+import { initialState } from "../reducers/ChatReducer"
+import { chatReducer } from "../reducers/ChatReducer"
+import { ChatContext } from "./ChatContext"
 
+
+export const ChatProvider = ({children}) => {
+
+const [state, dispatch] = useReducer(chatReducer,initialState)
+
+  return (
+    <ChatContext.Provider value={{state,dispatch}}>
+        {children}
+    </ChatContext.Provider>
+  )
+}
